74f1e968bb42e443a39cc81a9c2d0ac6dafda680
lezgoals <shailza728ss@gmail.com>
2020-11-15 18:58:36 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,158 @@
+var data = {
+	q: {
+		sound: new Howl({
+  		urls: ['sounds/quail.wav']
+		}),
+		color: '#1abc9c'
+	},
+	w: {
+		sound: new Howl({
+  		urls: ['sounds/weasel.wav']
+		}),
+		color: '#2ecc71'
+	},
+	e: {
+		sound: new Howl({
+  		urls: ['sounds/emu.wav']
+		}),
+		color: '#3498db'
+	},
+	r: {
+		sound: new Howl({
+  		urls: ['sounds/rhinos.wav']
+		}),
+		color: '#9b59b6'
+	},
+		t: {
+		sound: new Howl({
+  		urls: ['sounds/turkey.wav']
+		}),
+		color: '#34495e'
+	},
+	y: {
+		sound: new Howl({
+  		urls: ['sounds/yak.wav']
+		}),
+		color: '#16a085'
+	},
+	u: {
+		sound: new Howl({
+  		urls: ['sounds/unau.wav']
+		}),
+		color: '#27ae60'
+	},
+	i: {
+		sound: new Howl({
+  		urls: ['sounds/ibis.wav']
+		}),
+		color: '#2980b9'
+	},
+	o: {
+		sound: new Howl({
+			urls: ['sounds/orangutan.wav']
+		}),
+		color: '#8e44ad'
+	},
+	p: {
+		sound: new Howl({
+  		urls: ['sounds/platypus.wav']
+		}),
+		color: '#2c3e50'
+	},
+	a: {
+		sound: new Howl({
+  		urls: ['sounds/alpaca.wav']
+		}),
+		color: '#f1c40f'
+	},
+	s: {
+		sound: new Howl({
+  		urls: ['sounds/squirrel.wav']
+		}),
+		color: '#e67e22'
+	},
+		d: {
+		sound: new Howl({
+  		urls: ['sounds/duck.wav']
+		}),
+		color: '#e74c3c'
+	},
+	f: {
+		sound: new Howl({
+  		urls: ['sounds/fox.wav']
+		}),
+		color: '#95a5a6'
+	},
+	g: {
+		sound: new Howl({
+  		urls: ['sounds/guineaPig.wav']
+		}),
+		color: '#f39c12'
+	},
+	h: {
+		sound: new Howl({
+  		urls: ['sounds/humanChild.wav']
+		}),
+		color: '#d35400'
+	},
+	j: {
+		sound: new Howl({
+  		urls: ['sounds/jackal.wav']
+		}),
+		color: '#1abc9c'
+	},
+	k: {
+		sound: new Howl({
+  		urls: ['sounds/koala.wav']
+		}),
+		color: '#2ecc71'
+	},
+	l: {
+		sound: new Howl({
+  		urls: ['sounds/leopard.wav']
+		}),
+		color: '#3498db'
+	},
+	z: {
+		sound: new Howl({
+  		urls: ['sounds/zebra.wav']
+		}),
+		color: '#9b59b6'
+	},
+	x: {
+		sound: new Howl({
+  		urls: ['sounds/xantus.wav']
+		}),
+		color: '#34495e'
+	},
+	c: {
+		sound: new Howl({
+  		urls: ['sounds/coyote.wav']
+		}),
+		color: '#16a085'
+	},
+	v: {
+		sound: new Howl({
+  		urls: ['sounds/vulture.wav']
+		}),
+		color: '#27ae60'
+	},
+	b: {
+		sound: new Howl({
+  		urls: ['sounds/barnOwl.wav']
+		}),
+		color: '#2980b9'
+	},
+	n: {
+		sound: new Howl({
+			urls: ['sounds/narwhal.wav']
+		}),
+		color: '#8e44ad'
+	},
+	m: {
+		sound: new Howl({
+  		urls: ['sounds/meerkat.wav']
+		}),
+		color: '#2c3e50'
+	}
+}
